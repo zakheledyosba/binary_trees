03dfecb8bb6bb8e970f330658cf3e6b7f0c868c9
zakheledyosba <zakheledyosiba94@gmail.com>
2024-03-31 12:58:23 -0700

    binary tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,10 +2,12 @@
 
 
 /**
- * binary_tree_is_leaf- checks id the node id a leaf of a binary tree.
+ * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
  * @node: A pointer to the node to check.
- * Retrun: if the node is a leaf - 1,otherwise - 0.
-*/
+ *
+ * Return: If the node is a leaf - 1.
+ *         Otherwise - 0.
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,15 +2,17 @@
 
 
 /**
- * int binary_tree_is_root - checks if a node is a root of a binary tree.
- * @node: Apointer to the node to check.
- * Return: if the node is a root -1, otherwise -0.
-*/
+ * binary_tree_is_root - Checks if a node is a root of a binary tree.
+ * @node: A pointer to the node to check.
+ *
+ * Return: If the node is a root - 1.
+ *         Otherwise - 0.
+ */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
 		return (0);
-	return (1);
 
+	return (1);
 }
