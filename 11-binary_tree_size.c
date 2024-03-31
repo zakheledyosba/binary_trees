5b6f00f67d70db6bf4784bad9d573215e27e797a
zakheledyosba <zakheledyosiba94@gmail.com>
2024-03-31 12:40:29 -0700

    binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,10 +2,11 @@
 
 
 /**
- * size_t binary_tree_size - measures the depth of a binary tree.
- * @tree: is a pointer to the root node of the tree to measure the size.
- * Return: the size of the tree.
-*/
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the size of.
+ *
+ * Return: The size of the tree.
+ */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
